d17df240aace10e40989dcba854dd9a4c3a9e724
nsihor <nsihor@gmail.com>
2024-08-16 09:14:55 +0300

    add MenuLink

---
+++ src/components/MenuLink/MenuLink.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import spriteG from "../../images/svg/menu/symbol-defs-g.svg";
+import spriteW from "../../images/svg/menu/symbol-defs-w.svg";
+import styles from "./styles.module.scss";
+import {useState} from "react";
+
+const MenuLink = ({ page }) => {
+    const [hover, setHover] = useState(false)
+    const to = page === 'dashboard' ? '/' : `/${page}`;
+    const chosenSprite = (isActive, hover) => isActive || hover ? spriteW : spriteG;
+
+    return (
+        <NavLink className={styles.wrap} to={to}
+                 onMouseEnter={() => setHover(true)}
+                 onMouseLeave={() => setHover(false)}
+        >
+            {({ isActive }) => (
+                <div className={clsx(styles.link, isActive && styles.link_active)}>
+                    <svg className={styles.link__svg}>
+                        <use xlinkHref={`${chosenSprite(isActive, hover)}#icon-${page}`}></use>
+                    </svg>
+                    <span>{page.charAt(0).toUpperCase() + page.slice(1)}</span>
+                    {page !== 'dashboard' &&
+                    <svg className={styles.link__svg_arr}>
+                        <use xlinkHref={`${chosenSprite(isActive, hover)}#icon-m-arrow`}></use>
+                    </svg>}
+                </div>
+            )}
+        </NavLink>
+    );
+};
+
+export default MenuLink;
