96811450cb6a63f02fb4eb986a59b2f525137a92
nsihor <nsihor@gmail.com>
2024-08-18 16:52:44 +0300

    update greeting

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -1,11 +1,12 @@
 import styles from "./styles.module.scss"
 import Greeting from "../../components/Greeting/Greeting";
 import CustomersBlock from "../../components/CustomersBlock/CustomersBlock";
+import {users} from "../../constants";
 
 const Customers = () => (
     <div className={styles.container}>
         <div className={styles.container__greeting}>
-            <Greeting/>
+            <Greeting name={users[0].name}/>
         </div>
         <CustomersBlock/>
     </div>
