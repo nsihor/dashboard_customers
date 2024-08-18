8bcb4d8cd0690460f282014324ae8872f54c1f2f
nsihor <nsihor@gmail.com>
2024-08-18 17:51:21 +0300

    add pagination buttons

---
+++ src/components/PaginationButtons/PaginationButtons.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss"
+import PaginationBtn from "../PaginationBtn/PaginationBtn";
+
+const PaginationButtons = () => {
+    return (
+        <div className={styles.pgn}>
+        </div>
+    )
+}
+
+export default PaginationButtons;
