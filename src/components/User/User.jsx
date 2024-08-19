5ab0d8dae9348731dfb06c3f17f08e23480d2de4
nsihor <nsihor@gmail.com>
2024-08-19 03:32:31 +0300

    update alt

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss"
 const User = ({user}) => {
     return (
         <div className={styles.user}>
-            <img className={styles.user__photo} src={user.photo} alt="photo"/>
+            <img className={styles.user__photo} src={user.photo} alt="user"/>
             <div className={styles.user__info}>
                 <span className={styles.user__info__name}>{user.name}</span>
                 <span className={styles.user__info__role}>{user.role}</span>
