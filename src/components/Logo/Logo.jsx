aa6c2f321a83383e4315093640b13301a66e141c
nsihor <nsihor@gmail.com>
2024-08-19 03:20:20 +0300

    add rotate animation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,14 @@
 import {Link} from "react-router-dom";
 import logo from "../../../src/images/svg/logo.svg"
 import styles from "./styles.module.scss"
+import {useState} from "react";
 
 const Logo = () => {
+    const [rotate, setRotate] = useState(0)
+
     return (
-        <Link className={styles.title} to='/'>
-            <svg className={styles.title__svg} viewBox="0 0 37 37">
+        <Link className={styles.title} to='/' onMouseOver={() => setRotate((ps) => ps + 180)}>
+            <svg className={styles.title__svg} style={{ transform: `rotate(${rotate}deg)` }} viewBox="0 0 37 37">
                 <use xlinkHref={`${logo}#logo`}></use>
             </svg>
             <h1 className={styles.title__text}>Dashboard
